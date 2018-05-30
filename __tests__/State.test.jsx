16f452f05cc2792321245c4a292b43fe16014e9f
Alvaro Bernal <alvaro.bernal.gimeno@gmail.com>
2018-05-30 14:47:02 +0100

    Update snapshots / Fix test

--- __tests__/State.test.jsx
+++ __tests__/State.test.jsx
@@ -3,6 +3,6 @@ import renderer from "react-test-renderer";
 import { State } from "../src/";
 
 it("it matches the snapshot", () => {
-  const tree = renderer.create(<State> Hola </State>).toJSON();
+  const tree = renderer.create(<State value="hola"> Hola </State>).toJSON();
   expect(tree).toMatchSnapshot();
 });
