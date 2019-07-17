3785a4954734649a4890f7d625feb5991879c45c
Alvaro Bernal <alvaro.bernal.gimeno@gmail.com>
2019-07-17 22:14:24 +0100

    Refactoring: consistency

--- src/Item.jsx
+++ src/Item.jsx
@@ -3,7 +3,25 @@ import PropTypes from "prop-types";
 import "./Item.scss";
 
 const Item = props => {
-  const { active, tabIndex, className, disable, value, ...rest } = props;
+  Item.propTypes = {
+    value: PropTypes.string.isRequired,
+    children: PropTypes.node,
+    disable: PropTypes.bool,
+    active: PropTypes.bool,
+    tabIndex: PropTypes.number,
+    className: PropTypes.string
+  };
+
+  Item.defaultProps = {
+    children: "",
+    active: false,
+    disable: false,
+    tabIndex: 0,
+    className: ""
+  };
+
+  const { active, className, disable, value, ...rest } = props;
+  // console.log("Should be disable", props, disable);
 
   const classes = [
     "abg-switch__item",
@@ -14,33 +32,16 @@ const Item = props => {
 
   return (
     <span
-      tabIndex={tabIndex}
       className={classes}
       {...rest}
       role="radio"
       aria-checked={active}
+      aria-disabled={disable}
     >
       {props.children}
       <input type="radio" value={value} defaultChecked={active} />
     </span>
   );
-};
-
-Item.propTypes = {
-  value: PropTypes.string.isRequired,
-  children: PropTypes.node,
-  disable: PropTypes.bool,
-  active: PropTypes.bool,
-  tabIndex: PropTypes.number,
-  className: PropTypes.string
-};
-
-Item.defaultProps = {
-  children: "",
-  active: false,
-  disable: false,
-  tabIndex: 0,
-  className: ""
 };
 
 export default Item;
--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -3,6 +3,18 @@ import PropTypes from "prop-types";
 import "./Overlay.scss";
 
 const Overlay = props => {
+  Overlay.propTypes = {
+    goTo: PropTypes.number.isRequired,
+    items: PropTypes.number.isRequired,
+    classTransition: PropTypes.string,
+    className: PropTypes.string
+  };
+
+  Overlay.defaultProps = {
+    classTransition: "abg-switch__overlay-transition",
+    className: ""
+  };
+
   const { goTo, items, classTransition, className, ...rest } = props;
   const classes = [className, "abg-switch__overlay"].join(" ");
   return (
@@ -15,18 +27,6 @@ const Overlay = props => {
       {...rest}
     />
   );
-};
-
-Overlay.propTypes = {
-  goTo: PropTypes.number.isRequired,
-  items: PropTypes.number.isRequired,
-  classTransition: PropTypes.string,
-  className: PropTypes.string
-};
-
-Overlay.defaultProps = {
-  classTransition: "abg-switch__overlay-transition",
-  className: ""
 };
 
 export default Overlay;
